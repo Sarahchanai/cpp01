076e2782c6a6884f35f75ee4926dc33aabe2f0e9
Sarahchanai <sarahh.chanai@gmail.com>
2025-11-21 17:38:22 +0100

    a terminer

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sachanai <sachanai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/21 11:23:04 by sachanai          #+#    #+#             */
+/*   Updated: 2025/11/21 17:21:32 by sachanai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+ 
+#include <string>
+#include <iostream>
+
+//foo survit -> heap
+//bart meurt vite -> stack
+
+
+
+
+
+
+
+#endif
