8cda369d0f6473c49f6346d3fc78f996141b054d
Sarahchanai <sarahh.chanai@gmail.com>
2026-03-04 13:30:18 +0100

    finish

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sachanai <sachanai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sarah <sarah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 11:58:48 by sachanai          #+#    #+#             */
-/*   Updated: 2025/12/13 15:15:07 by sachanai         ###   ########.fr       */
+/*   Updated: 2026/03/04 13:29:43 by sarah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,50 +40,53 @@ Harl::~Harl(void)
 
 void	Harl::debug(void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
-void    Harl::info(void)
+void	Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void    Harl::warning(void)
+void	Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for"
-    <<"years, whereas you started working here just last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
-void    Harl::error(void)
+void	Harl::error(void)
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 
-void Harl::complain(std::string level)
+//L'idée ds complain() -> avoir 1 tableau qui fait le lien btw le nom du niveau & la fonc° :
+
+void	Harl::complain(std::string inputLevel)
 {
-    void (Harl::*fonctions[4])(void) = 
-    { //propre au c++ -> pour initialiser
-        &Harl::debug,    // ← 0
-        &Harl::info,     // ← 1
-        &Harl::warning,  // ← 2
-        &Harl::error     // ← 3
-    };
-    
-    std::string niveaux[4] = 
-    {
-        "DEBUG",    // ← 0 : MÊME INDICE que debug
-        "INFO",     // ← 1 : MÊME INDICE que info
-        "WARNING",  // ← 2 : MÊME INDICE que warning
-        "ERROR"     // ← 3 : MÊME INDICE que error
-    };
+	void (Harl::*functions[4])(void) = 
+	{ //propre au c++ -> pour initialiser
+		&Harl::debug,    // -> 0
+		&Harl::info,     // -> 1
+		&Harl::warning,  // -> 2
+		&Harl::error     // -> 3
+	};
+
+	std::string levelNames[4] = 
+	{
+		"DEBUG", // -> 0 : ^m INDICE que debug
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
     
     // Boucle pour trouver l'indice correspondant
     for (int i = 0; i < 4; i++) 
     {
-        if (level == niveaux[i]) {
-            (this->*fonctions[i])();  // Appelle la fonction au MÊME indice
-            return;
-        }
-    }
+        if ( inputLevel== levelNames[i])
+		{
+			(this->*functions[i])();  // Appelle la fonction au MÊME indice
+			return;
+		}
+	}
+	std::cerr << "Unknown level : " << inputLevel << std::endl;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -3,22 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sachanai <sachanai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sarah <sarah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 11:58:29 by sachanai          #+#    #+#             */
-/*   Updated: 2025/12/13 15:14:20 by sachanai         ###   ########.fr       */
+/*   Updated: 2026/03/04 13:26:19 by sarah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int    main(void)
-{
-   Harl lewis;
-   lewis.complain("INFO");
-   std::cout << std::endl;
-   lewis.complain("WARNING");
+// int	main(void)
+// {
+//    Harl	lewis;
+   
+//    lewis.complain("DEBUG");
+//    std::cout << std::endl;
+//    lewis.complain("INFO");
+//    std::cout << std::endl;
+//    lewis.complain("WARNING");
+//    std::cout << std::endl;
+//    lewis.complain("ERROR");
+//    std::cout << std::endl;
 
-   
-   
+//    return (0);
+
+// }
+
+
+int	main(int ac, char **av)
+{
+	Harl	lewis;
+
+	if (ac != 2)
+	{
+		std::cerr << "Usage: ./harl <level>" << std::endl;
+		return (1);
+    }
+	lewis.complain(av[1]);
+    return (0);
 }
